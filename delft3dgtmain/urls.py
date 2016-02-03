ec6c2fd493283d46e1429544bfab22863b6a71a9
Tijn Schmits <tijn.schmits@deltares.nl>
2016-02-03 11:52:58 +0100

    makes all paths in frontend relative

--- delft3dgtmain/urls.py
+++ delft3dgtmain/urls.py
@@ -21,8 +21,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+
     url(r'^admin/', admin.site.urls),
 
     url(r'^', include('delft3dworker.urls')),
     url(r'^', include('delft3dgtfrontend.urls')),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
