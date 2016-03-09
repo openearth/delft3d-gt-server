82febfef798afbab95271c64685dd5ee635acf7e
Tijn Schmits <tijn.schmits@deltares.nl>
2016-03-09 09:53:48 +0100

    integrates mock and makes test run successfully

--- delft3dworker/tests.py
+++ delft3dworker/tests.py
@@ -7,14 +7,19 @@ Replace this with more appropriate tests for your application.
 
 import os
 
+# django test imports
 from django.test import TestCase
 from django.test import modify_settings
 
+# runner imports
 from djcelery.contrib.test_runner import CeleryTestSuiteRunner
 from teamcity.unittestpy import TeamcityTestRunner
 
-from delft3dgtmain import settings
+# mock imports
+from mock import patch, MagicMock
+from docker import Client
 
+from delft3dgtmain import settings
 from delft3dworker.tasks import rundocker
 
 
@@ -29,12 +34,19 @@ class SimpleTest(TestCase):
         Test that we can run rundocker.delay with succesful result
         """
 
-        # result = rundocker.delay(
-        #     settings.DELFT3D_IMAGE_NAME, 
-        #     'ed7a1f3c-e46a-4eb3-9ea3-3d1729a69562', 
-        #     '/data/container/files/'
-        # )
+        container = MagicMock()
+        container.get.return_value = 'id'
 
-        # self.assertTrue(result.successful())
+        with patch('docker.Client') as MockClient:
+            MockClient.create_client.return_value = container
 
-        self.assertTrue(True)
+            # TODO: Make this delay task work with the mocked docker Client
+
+            # result = rundocker.delay(
+            #     settings.DELFT3D_IMAGE_NAME, 
+            #     'ed7a1f3c-e46a-4eb3-9ea3-3d1729a69562', 
+            #     '/data/container/files/'
+            # )
+
+            # MockClient.create_client.assert_called_once_with()
+            self.assertTrue(True)
