b2663c11fdc593a47228b62236ec06eb914c1923
Tijn Schmits <tijn.schmits@deltares.nl>
2016-03-10 13:43:14 +0100

    cleans up models and views

--- delft3dworker/models.py
+++ delft3dworker/models.py
@@ -1,8 +1,5 @@
 import os
 
-from shutil import copytree, copystat, rmtree
-from datetime import datetime
-
 from django.conf import settings  #noqa
 from django.db import models  #noqa
 from jsonfield import JSONField
@@ -10,73 +7,10 @@ from jsonfield import JSONField
 from celery.result import AsyncResult
 from celery.contrib.abortable import AbortableAsyncResult
 
-from mako.template import Template
-
 from delft3dworker.tasks import donothing
 from delft3dworker.tasks import postprocess
 from delft3dworker.tasks import process
 from delft3dworker.tasks import simulate
-
-
-# ################################### Sprint 1 Architecture
-
-class Delft3DWorker(models.Model):
-    uuid = models.CharField(max_length=256, editable=False)
-    workerid = models.CharField(max_length=256, editable=False)
-    workingdir = models.CharField(max_length=256, editable=False)
-    fileurl = models.CharField(max_length=256, editable=False)
-
-    name = models.CharField(max_length=256)
-    status = models.CharField(max_length=256)
-    info = models.CharField(max_length=256)
-    json = JSONField()
-
-    def save(self, *args, **kwargs):
-        self.workingdir = "{0}/{1}/".format(settings.WORKER_FILEDIR, self.uuid)
-        self.fileurl = "{0}/{1}/".format(settings.WORKER_FILEURL, self.uuid)
-        self._create_model_schema()
-        super(Delft3DWorker, self).save(*args, **kwargs)
-
-    def delete(self, *args, **kwargs):
-        if os.path.exists(self.workingdir):
-            rmtree(self.workingdir)
-        super(Delft3DWorker, self).delete(*args, **kwargs)
-
-    def __unicode__(self):
-        return "{0} - {1}".format(self.name, self.uuid)
-
-    def _create_model_schema(self):
-
-        if not os.path.exists(self.workingdir):
-            copytree('/data/container/delft3ddefaults', self.workingdir)
-
-        # create input dict for template renderer
-        time_format = "%Y-%m-%d %H:%M:%S"
-        input_dict =   {
-            "discharge": 1250,
-            "dt": self.json["dt"],
-            "his_interval": 1800,
-            "map_interval": 7200,
-            "reference_time": "2013-12-01 00:00:00",
-            "Tstart": "2014-01-01 00:00:00",
-            "Tstop": "2015-01-01 00:00:00"
-        }
-
-        ref_time = datetime.strptime(input_dict['reference_time'], time_format)
-        start_time = datetime.strptime(input_dict['Tstart'], time_format)
-        stop_time = datetime.strptime(input_dict['Tstop'], time_format)
-
-        input_dict['reference_time'] = datetime.strftime(ref_time, "%Y-%m-%d")
-        input_dict['Tstart'] = (start_time - ref_time).total_seconds()/60
-        input_dict['Tstop'] = (stop_time - ref_time).total_seconds()/60
-
-        # render and write a.mdf
-        mdf_template_file = os.path.join('/data/container/delft3dtemplates', 'a.mdf')
-        mdf_template = Template(filename=mdf_template_file)
-        rendered_schema = mdf_template.render(**input_dict).replace('\r\n','\n')
-        with open(os.path.join(self.workingdir, 'a.mdf'), 'w') as output:
-            output.write(rendered_schema)
-
 
 
 # ################################### Sprint 2 Architecture
@@ -89,8 +23,8 @@ class Scene(models.Model):
     fileurl = models.CharField(max_length=256)
 
     name = models.CharField(max_length=256)
-    state = models.CharField(max_length=256)
-    info = models.CharField(max_length=256)
+    state = models.CharField(max_length=256, blank=True)
+    info = models.CharField(max_length=256, blank=True)
 
     def simulate(self):
         try:
@@ -118,8 +52,8 @@ class CeleryTask(models.Model):
     Celery Task model
     """
     uuid = models.CharField(max_length=256)
-    state = models.CharField(max_length=256)
-    state_meta = JSONField()
+    state = models.CharField(max_length=256, blank=True)
+    state_meta = JSONField(blank=True)
 
     def serialize(self):
         # update state
@@ -194,3 +128,68 @@ class SimulationTask(CeleryTask):
 
     def __unicode__(self):
         return "{0} - {1} - {2}".format(self.scene, self.uuid, self.state)
+
+
+
+
+# ################################### Sprint 1 Architecture
+from shutil import copytree, copystat, rmtree
+from datetime import datetime
+from mako.template import Template
+
+class Delft3DWorker(models.Model):
+    uuid = models.CharField(max_length=256, editable=False)
+    workerid = models.CharField(max_length=256, editable=False)
+    workingdir = models.CharField(max_length=256, editable=False)
+    fileurl = models.CharField(max_length=256, editable=False)
+
+    name = models.CharField(max_length=256)
+    status = models.CharField(max_length=256)
+    info = models.CharField(max_length=256)
+    json = JSONField()
+
+    def save(self, *args, **kwargs):
+        self.workingdir = "{0}/{1}/".format(settings.WORKER_FILEDIR, self.uuid)
+        self.fileurl = "{0}/{1}/".format(settings.WORKER_FILEURL, self.uuid)
+        self._create_model_schema()
+        super(Delft3DWorker, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if os.path.exists(self.workingdir):
+            rmtree(self.workingdir)
+        super(Delft3DWorker, self).delete(*args, **kwargs)
+
+    def __unicode__(self):
+        return "{0} - {1}".format(self.name, self.uuid)
+
+    def _create_model_schema(self):
+
+        if not os.path.exists(self.workingdir):
+            copytree('/data/container/delft3ddefaults', self.workingdir)
+
+        # create input dict for template renderer
+        time_format = "%Y-%m-%d %H:%M:%S"
+        input_dict =   {
+            "discharge": 1250,
+            "dt": self.json["dt"],
+            "his_interval": 1800,
+            "map_interval": 7200,
+            "reference_time": "2013-12-01 00:00:00",
+            "Tstart": "2014-01-01 00:00:00",
+            "Tstop": "2015-01-01 00:00:00"
+        }
+
+        ref_time = datetime.strptime(input_dict['reference_time'], time_format)
+        start_time = datetime.strptime(input_dict['Tstart'], time_format)
+        stop_time = datetime.strptime(input_dict['Tstop'], time_format)
+
+        input_dict['reference_time'] = datetime.strftime(ref_time, "%Y-%m-%d")
+        input_dict['Tstart'] = (start_time - ref_time).total_seconds()/60
+        input_dict['Tstop'] = (stop_time - ref_time).total_seconds()/60
+
+        # render and write a.mdf
+        mdf_template_file = os.path.join('/data/container/delft3dtemplates', 'a.mdf')
+        mdf_template = Template(filename=mdf_template_file)
+        rendered_schema = mdf_template.render(**input_dict).replace('\r\n','\n')
+        with open(os.path.join(self.workingdir, 'a.mdf'), 'w') as output:
+            output.write(rendered_schema)
--- delft3dworker/tasks.py
+++ delft3dworker/tasks.py
@@ -1,10 +1,9 @@
 from __future__ import absolute_import
 
-import signal
+import time 
 
 from celery import shared_task
 from celery.contrib.abortable import AbortableTask
-from celery.signals import task_revoked
 from celery.utils.log import get_task_logger
 
 from docker import Client
@@ -12,7 +11,53 @@ from docker import Client
 logger = get_task_logger(__name__)
 
 
-# ################################### Sprint 1 Architecture
+# ################################### Sprint 2 Architecture
+
+@shared_task(bind=True, base=AbortableTask)
+def donothing(self):
+    logger.info('Starting task "donothing"...')    
+    state_meta = {}
+    self.update_state(state='DOINGNOTHING', meta=state_meta)
+    time.sleep(20)
+    logger.info('... task "donothing" Finished')    
+    return state_meta
+
+@shared_task(bind=True, base=AbortableTask)
+def postprocess(self):
+    logger.info('Starting task "postprocess"...')    
+    state_meta = {'key': 'value',}
+    self.update_state(state='POSTPROCESSING', meta=state_meta)
+    time.sleep(20)
+    logger.info('... task "postprocess" Finished')    
+    return state_meta
+
+
+@shared_task(bind=True, base=AbortableTask)
+def process(self):
+    logger.info('Starting task "process"...')    
+    state_meta = {
+        'channel_network_image': '/sampledata/example_channel_network.png',
+        'delta_fringe_image': '/sampledata/example_delta_fringe.png',
+        'logfile': '/sampledata/logfile.f34',
+    }
+    self.update_state(state='PROCESSING', meta=state_meta)
+    time.sleep(20)
+    logger.info('... task "process" Finished')    
+    return state_meta
+
+@shared_task(bind=True, base=AbortableTask)
+def simulate(self):
+    logger.info('Starting task "simulate"...')    
+    state_meta = {'key': 'value',}
+    self.update_state(state='SIMULATING', meta=state_meta)
+    time.sleep(20)
+    logger.info('... task "simulate" Finished')    
+    return state_meta
+
+
+
+
+# ################################### Sprint 1 Architecture (deprecated)
 
 @shared_task(bind=True, base=AbortableTask)
 def rundocker(self, name, uuid, workingdir):
@@ -90,41 +135,3 @@ class DockerRun():
 
         self.container = self.client.create_container(self.name, host_config=config)
         self.id = self.container.get('Id')
-
-
-
-# ################################### Sprint 2 Architecture
-import time 
-
-@shared_task(bind=True, base=AbortableTask)
-def donothing(self):
-    state_meta = {}
-    self.update_state(state='DOINGNOTHING', meta=state_meta)
-    time.sleep(20)
-    return state_meta
-
-@shared_task(bind=True, base=AbortableTask)
-def postprocess(self):
-    state_meta = {'key': 'value',}
-    self.update_state(state='POSTPROCESSING', meta=state_meta)
-    time.sleep(20)
-    return state_meta
-
-
-@shared_task(bind=True, base=AbortableTask)
-def process(self):
-    state_meta = {
-        'channel_network_image': '/sampledata/example_channel_network.png',
-        'delta_fringe_image': '/sampledata/example_delta_fringe.png',
-        'logfile': '/sampledata/logfile.f34',
-    }
-    self.update_state(state='PROCESSING', meta=state_meta)
-    time.sleep(20)
-    return state_meta
-
-@shared_task(bind=True, base=AbortableTask)
-def simulate(self):
-    state_meta = {'key': 'value',}
-    self.update_state(state='SIMULATING', meta=state_meta)
-    time.sleep(20)
-    return state_meta
