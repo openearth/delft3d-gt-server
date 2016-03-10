9eddecb2c884f1dee54b8621576392ea2dae2a9a
Tijn Schmits <tijn.schmits@deltares.nl>
2016-03-10 13:48:27 +0100

    cleans up models and views

--- delft3dworker/views.py
+++ delft3dworker/views.py
@@ -1,5 +1,58 @@
 from __future__ import absolute_import
 
+from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import View
+
+from json_views.views import JSONDetailView
+from json_views.views import JSONListView
+
+from delft3dworker.models import Scene
+
+
+# ################################### Sprint 2 Architecture
+
+class SceneCreateView(CreateView):    
+    model = Scene
+    fields = ['name', 'state', 'info']
+    success_url = reverse_lazy('run_list')
+
+
+class SceneDeleteView(DeleteView):    
+    model = Scene
+    success_url = reverse_lazy('run_list')
+
+
+class SceneDetailView(JSONDetailView):    
+    model = Scene
+
+
+class SceneListView(JSONListView):    
+    model = Scene
+
+
+class SceneStartView(View):    
+    model = Scene
+
+    # TODO: remove get
+    def get(self, request, *args, **kwargs):
+        pk = kwargs['pk']
+        scene = get_object_or_404(Scene, pk=pk)
+        return HttpResponse(scene.simulate())
+
+    def post(self, request, *args, **kwargs):
+        pk = kwargs['pk']
+        scene = get_object_or_404(Scene, pk=pk)
+        return HttpResponse(scene.simulate())
+
+
+
+
+# ################################### Sprint 1 Architecture
 import json
 import uuid
 import random
@@ -9,17 +62,13 @@ from celery.contrib.abortable import AbortableAsyncResult
 
 from django.conf import settings
 from django.core import serializers
-from django.core.urlresolvers import reverse_lazy
-from django.http import HttpResponse
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
-from django.views.generic import CreateView, DeleteView, View
-
-from json_views.views import JSONDetailView, JSONListView
 
 from delft3dgtmain.celery import app
-from delft3dworker.models import Delft3DWorker, Scene, SimulationTask
+from delft3dworker.models import Delft3DWorker
+from delft3dworker.models import SimulationTask
+
 from delft3dworker.tasks import rundocker
 
 
@@ -34,8 +83,6 @@ ERROR = 'error'
 RUNNING = 'running'
 SUCCESS = 'success'
 
-
-# ################################### Sprint 1 Architecture
 
 def runs(request):
 
@@ -139,33 +186,3 @@ def dorun(request):
         }
 
     return HttpResponse(json.dumps(data), content_type = 'application/json; charset=utf8')
-
-
-# ################################### Sprint 2 Architecture
-
-class SceneCreateView(CreateView):    
-    model = Scene
-    fields = ['name', 'state', 'info']
-    success_url = reverse_lazy('run_list')
-
-
-class SceneDeleteView(DeleteView):    
-    model = Scene
-    success_url = reverse_lazy('run_list')
-
-
-class SceneDetailView(JSONDetailView):    
-    model = Scene
-
-
-class SceneListView(JSONListView):    
-    model = Scene
-
-
-class SceneStartView(View):    
-    model = Scene
-
-    def get(self, request, *args, **kwargs):
-        pk = kwargs['pk']
-        scene = get_object_or_404(Scene, pk=pk)
-        return HttpResponse(scene.simulate())
