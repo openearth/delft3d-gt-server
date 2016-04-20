17118095bf33abe5fab1651d729870d40659c018
Tijn Schmits <tijn.schmits@deltares.nl>
2016-04-20 11:00:45 +0200

    merges a double migration file

---
+++ delft3dworker/migrations/0018_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('delft3dworker', '0017_template_groups'),
+        ('delft3dworker', '0014_scene_task_id'),
+    ]
+
+    operations = [
+    ]
