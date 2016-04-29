7a761e88210e6984a9949f4d83eb8698ce6e8f2c
Maarten Pronk <git@evetion.nl>
2016-04-29 11:46:59 +0200

    Cleanup

---
+++ delft3dworker/management/commands/cleanup.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings  # noqa
+
+from delft3dworker.models import Scene
+
+from os import listdir, walk
+from os.path import join, dirname, split
+from shutil import rmtree
+
+class Command(BaseCommand):
+    help = 'Removes leftover file directories not linked to existing models.'
+
+    def handle(self, *args, **options):
+            ignore = set([join(settings.WORKER_FILEDIR,'theme')])  # files styling dir
+            scenedirs = [scene.workingdir.encode('ascii','ignore') for scene in Scene.objects.all()]
+            linked = set()
+
+            # Django workingdirs have trailing slash
+            for scenedir in scenedirs:
+                if scenedir[-1] == '/':
+                    scenedir = scenedir[:-1]
+                linked.add(scenedir)
+
+            r, d, f = walk(settings.WORKER_FILEDIR).next()
+            existing = set([join(r,directory) for directory in d])
+
+            linked = linked | ignore
+            unlinked = existing - linked
+
+            for directory in unlinked:
+                try:
+                    # rmtree(directory)
+                    self.stdout.write('Successfully deleted unlinked folder {}'.format(directory))
+                except:
+                    self.stdout.write("Couldn't delete folder {}".format(directory))
