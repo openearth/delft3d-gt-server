dc3ff9a5caf83b23609af2a7e56706fbb8b605b2
Maarten Pronk <git@evetion.nl>
2016-06-06 17:42:55 +0200

    EOF

--- delft3dworker/permissions.py
+++ delft3dworker/permissions.py
@@ -14,4 +14,3 @@ class ViewObjectPermissions(permissions.DjangoObjectPermissions):
         'PATCH': ['%(app_label)s.change_%(model_name)s'],
         'DELETE': ['%(app_label)s.delete_%(model_name)s'],
     }
-    
