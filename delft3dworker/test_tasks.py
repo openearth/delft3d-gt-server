dc699543f6db7f7f3956465e27df5a3927c05f77
Maarten Pronk <git@evetion.nl>
2016-06-06 18:25:32 +0200

    Can now test tasks with mocked DockerClient

--- delft3dworker/test_tasks.py
+++ delft3dworker/test_tasks.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf import settings
 from django.test import TestCase
 
@@ -19,11 +21,38 @@ class TaskTest(TestCase):
         settings.CELERY_ALWAYS_EAGER = True
         current_app.conf.CELERY_ALWAYS_EAGER = True
 
+        settings.DELFT3D_DUMMY_IMAGE_NAME = 'dummy_simulation'
+        settings.POSTPROCESS_DUMMY_IMAGE_NAME = 'dummy_postprocessing'
+        settings.PREPROCESS_DUMMY_IMAGE_NAME = 'dummy_preprocessing'
+        settings.PROCESS_DUMMY_IMAGE_NAME = 'dummy_processing'
+        settings.EXPORT_DUMMY_IMAGE_NAME = 'dummy_export'
+
     def test_dummy(self):
         delay = dummy.delay()
         self.assertTrue(delay.result is None)
 
+    def getFalse(self):
+        return False
+
     @patch('delft3dworker.tasks.DockerClient', autospec=True)
     def test_chainedtask(self, mockDockerClient):
-        parameters = ['.', {}, 'main']
-        delay = chainedtask.delay(self.parameters, self.workingdir, workflow)
+        # Autospec cant do init
+        mockDockerClient.return_value.id = '1238761287361'
+        mockDockerClient.return_value.running = self.getFalse
+
+        # No workflow given
+        parameters = [{}, os.getcwd(), '']
+        delay = chainedtask.delay(*parameters)
+        self.assertTrue(delay.result is None)
+
+        # Export workflow
+        parameters = [{}, os.getcwd(), 'export']
+        delay = chainedtask.delay(*parameters)
+        self.assertTrue("result" in delay.result)
+        self.assertTrue(delay.result['result'] == "Finished")
+
+        # Main workflow
+        parameters = [{}, os.getcwd(), 'main']
+        delay = chainedtask.delay(*parameters)
+        self.assertTrue("result" in delay.result)
+        self.assertTrue(delay.result['result'] == "Finished")
