1ac967df2014e9c453be843eb57f51a6efc3f007
Anna Van Gils <anna.vangils@deltares.nl>
2016-06-09 19:32:10 +0200

    add migrations

---
+++ delft3dworker/migrations/0037_auto_20160608_2304.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('delft3dworker', '0036_auto_20160527_1445'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='scene',
+            name='scenario',
+            field=models.ManyToManyField(to='delft3dworker.Scenario'),
+        ),
+    ]
