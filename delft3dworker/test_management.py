f413163b81c1045be3fa19272d5ec7ced9bb88ec
Maarten Pronk <git@evetion.nl>
2016-08-26 09:25:17 +0200

    Fixed test of command.

--- delft3dworker/test_management.py
+++ delft3dworker/test_management.py
@@ -1,13 +1,16 @@
 from __future__ import absolute_import
 
 from django.core.management import call_command
+import django.core.management
+
 from django.test import TestCase
 
 from mock import patch
 
+from StringIO import StringIO
+
 from delft3dworker.models import Scene
 from delft3dworker.models import Container
-
 
 
 class ManagementTest(TestCase):
@@ -17,26 +20,40 @@ class ManagementTest(TestCase):
 
     def setUp(self):
         self.scene = Scene.objects.create(
-                    name='Scene',
-                )
-        self.container = Container.objects.create(
-                    scene=self.scene,
-                    docker_id='abcdefg'
-                )
-        self.container = Container.objects.create(
-                    scene=self.scene,
-                    docker_id=''
-                )
-        self.container = Container.objects.create(
-                    scene=self.scene,
-                    docker_id='hijklmn'
-                )
+            name='Scene',
+        )
+        self.container_1_1 = Container.objects.create(
+            scene=self.scene,
+            docker_id='abcdefg'
+        )
+        self.container_1_0 = Container.objects.create(
+            scene=self.scene,
+            docker_id=''
+        )
+        self.container_0_1 = Container.objects.create(
+            scene=self.scene,
+            docker_id='hijklmn'
+        )
 
+    @patch("""delft3dworker.management.commands.
+        containersync_sceneupdate.Container._update_state_and_save""")
     @patch('delft3dcontainermanager.tasks.Client', **mock_options)
-    def test_containersync_sceneupdate(self, mockClient):
+    def test_containersync_sceneupdate(self, mockClient, mockContainerupdate):
         """
         Test match matrix for docker containers and model containers
         """
-        mockClient.return_value = [{u'Id': u'abcdefg'},{u'Id': u'opqrstu'}]
-        call_command('containersync_sceneupdate')
+        client = mockClient.return_value
+        client.containers.return_value = [{'Id': 'abcdefg'},
+                                          {'Id': 'orphan'}]
 
+        out = StringIO()
+        call_command('containersync_sceneupdate', stderr=out)
+
+        # Docker container not in database
+        self.assertIn(
+            'Docker container orphan not found in database!', out.getvalue())
+        client.remove_container.assert_called_with(
+            container='orphan', force=True)
+
+        # Docker container in database
+        mockContainerupdate.assert_called_with({'Id': 'abcdefg'})
