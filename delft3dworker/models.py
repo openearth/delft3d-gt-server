897893004c7b8aaa1714dfb2401259fc9d4e3743
Maarten Pronk <git@evetion.nl>
2016-09-07 15:01:35 +0200

    Added new container type.

--- delft3dworker/models.py
+++ delft3dworker/models.py
@@ -862,6 +862,7 @@ class Container(models.Model):
         ('process', 'process'),
         ('postprocess', 'postprocess'),
         ('export', 'export'),
+        ('sync_cleanup', 'sync_cleanup'),
     )
 
     container_type = models.CharField(
@@ -1084,6 +1085,8 @@ class Container(models.Model):
         prodir = os.path.join(workingdir, 'process')
         posdir = os.path.join(workingdir, 'postprocess')
         expdir = os.path.join(workingdir, 'export')
+        syndir = workingdir
+        datadir = os.path.abspath(os.path.join(workingdir, os.pardir))
 
         # Specific settings for each container type
         # TODO It would be more elegant to put these
@@ -1137,6 +1140,17 @@ class Container(models.Model):
                                        simdir],
                            'command': "/data/run.sh /data/svn/scripts/"
                            "preprocessing/preprocessing.py"
+                           },
+
+            'sync_cleanup': {'image': settings.SYNC_CLEANUP_IMAGE_NAME,
+                           'volumes': [
+                               '{0}:/data/:z'.format(datadir)],
+                           'environment': {"uuid": str(self.scene.suid),
+                                           "folder": syndir},
+                           'name': "{}-{}".format(self.container_type,
+                                                  str(self.scene.suid)),
+                           'folders': [],  # sync doesn't need new folders
+                           'command': "/data/run.sh cleanup"
                            },
 
             'process': {'image': settings.PROCESS_IMAGE_NAME,
