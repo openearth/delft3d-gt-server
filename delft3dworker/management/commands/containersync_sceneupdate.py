51c2e5d5bf3f6d6b7563d8d33dfad97d6586dc62
Tijn Schmits <tijn.schmits@deltares.nl>
2016-11-29 13:13:45 +0100

    adds a righteous way of starting simulations during a heartbeat: in order of start buttons pressed

--- delft3dworker/management/commands/containersync_sceneupdate.py
+++ delft3dworker/management/commands/containersync_sceneupdate.py
@@ -123,7 +123,10 @@ class Command(BaseCommand):
         shift Scene phase
         """
 
-        for scene in Scene.objects.all():
+        # ordering is done on start date (first, and id second):
+        # if a simulation slot is available, we want simulations to start
+        # in order of their date_started
+        for scene in Scene.objects.all().order_by('date_started','id'):
             scene.update_and_phase_shift()
 
     def _fix_container_state_mismatches_or_log(self):
