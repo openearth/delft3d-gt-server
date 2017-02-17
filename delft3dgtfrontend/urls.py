d79206fbc523716ae4841ab6086fc9d68fd8a662
Tijn Schmits <tijn.schmits@deltares.nl>
2017-02-17 13:34:21 +0100

    adds new password retrieval views

--- delft3dgtfrontend/urls.py
+++ delft3dgtfrontend/urls.py
@@ -5,18 +5,36 @@ from django.views.static import serve
 
 from django.contrib.auth.views import login
 from django.contrib.auth.views import logout
+from django.contrib.auth.views import password_reset
+from django.contrib.auth.views import password_reset_done
 
 urlpatterns = (
 
     # Login
-    url(r'^login/$', login, {'template_name': 'login.html'}),
+    url(r'^login/$', login, {'template_name': 'login.html'},
+        name="login"),
     url(r'^login/(?P<path>.*)$', serve, {
         'document_root': settings.STATIC_ROOT
     }),
 
     # Logout
-    url(r'^logout/$', logout, {'template_name': 'login.html'}),
+    url(r'^logout/$', logout, {'template_name': 'login.html'},
+        name="logout"),
     url(r'^logout/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT
+    }),
+
+    # Password Reset
+    url(r'^forgot/$', password_reset, {'template_name': 'forgot.html'},
+        name="password_reset"),
+    url(r'^forgot/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT
+    }),
+
+    # Password Reset Done
+    url(r'^done/$', password_reset_done, {'template_name': 'done.html'},
+        name="password_reset_done"),
+    url(r'^done/(?P<path>.*)$', serve, {
         'document_root': settings.STATIC_ROOT
     }),
 
