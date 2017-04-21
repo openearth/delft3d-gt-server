663260ad48f2ae3e90d254ee1133445e95e00eb2
Tijn Schmits <tijn.schmits@deltares.nl>
2017-04-21 14:05:00 +0200

    adds custom 403 and 404 error handlers

--- delft3dgtfrontend/views.py
+++ delft3dgtfrontend/views.py
@@ -1,3 +1,34 @@
 """
 Views for the ui.
 """
+import os.path
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import RequestContext
+
+
+def handler403(request):
+    filename = 'errorpages/403.html'
+
+    styled_page = os.path.join(settings.STATIC_ROOT, '403.html')
+    if os.path.isfile(styled_page):
+        filename = styled_page
+
+    r = render(request, filename,
+        {}, context_instance=RequestContext(request))
+    r.status_code = 403
+    return r
+
+def handler404(request):
+    filename = 'errorpages/404.html'
+
+    styled_page = os.path.join(settings.STATIC_ROOT, '404.html')
+    if os.path.isfile(styled_page):
+        filename = styled_page
+
+    r = render(request, filename,
+        {}, context_instance=RequestContext(request))
+    r.status_code = 403
+    return r
