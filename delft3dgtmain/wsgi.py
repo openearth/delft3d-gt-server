94cb58d97e8797de2371dcfcb33fe78a6f0c2f1b
Maarten Pronk <git@evetion.nl>
2018-02-07 16:54:26 +0100

    Removed check password function !

--- delft3dgtmain/wsgi.py
+++ delft3dgtmain/wsgi.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-# Import settings before importing db, auth, or it will fail
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "delft3dgtmain.settings")
 
-from django.contrib.auth.handlers.modwsgi import check_password
-
-from django.core.handlers.wsgi import WSGIHandler
-application = WSGIHandler()
+application = get_wsgi_application()
