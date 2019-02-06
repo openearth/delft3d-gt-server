4c31b6e511e5e7684ae49e5e99df84d13d0192b4
Maarten Pronk <git@evetion.nl>
2019-02-06 13:10:29 +0100

    839 Add migration management script.

---
+++ delft3dworker/management/commands/migrate_legacy_scenes.py
@@ -0,0 +1,48 @@
+from django.core.management import BaseCommand
+from delft3dworker.models import Scene
+from delft3dworker.models import Workflow
+import logging
+
+"""
+Script to migrate legacy scenes:
+- Adds workflows to scenes without
+- Corrects non-existing phases to existing ones
+
+The new kubernetes architecture has two new models,
+while the old svn model is gone. Migrating the legacy
+production database involves:
+- Run migrations
+- Add shortname and yaml file to template
+- Run this script
+- Start models where applicable
+"""
+
+
+class Command(BaseCommand):
+    help = "Scan for old Scenes and update them to the new ET architecture."
+
+    def handle(self, *args, **options):
+
+        # STEP I : Find scenes without a workflow
+        legacy_scenes = Scene.objects.filter(workflow=None)
+
+        # STEP II : Call local scan
+        for scene in legacy_scenes:
+
+            if scene.scenario.first() is None:
+                logging.warning("Scene {} has no scenario!".format(scene.id))
+                continue
+
+            logging.info("Migrating {} to idle.".format(scene.id))
+            workflow = Workflow.objects.create(
+                scene=scene,
+                name="{}-{}".format(scene.scenario.first().template.shortname, scene.suid),
+                progress=scene.progress,
+                version=scene.scenario.first().template.versions.first()  # latest
+            )
+            workflow.save()
+
+        for scene in Scene.objects.all():
+            if scene.phase not in Scene.phases:
+                logging.info("Migrating {} to idle.".format(scene.id))
+                scene.shift_to_phase(Scene.phases.idle)
